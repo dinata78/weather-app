6ac4f4edc309ad8b48d286ee8a4b82a4f19461e6
dinata78 <stevendinata78@gmail.com>
2024-12-01 17:40:34 +0700

    Remove dev-mode's console log

--- scripts/setWeeklyInfo.js
+++ scripts/setWeeklyInfo.js
@@ -3,7 +3,6 @@ export { setWeeklyInfo };
 function translateDate(dateInNum) {
   const [ , month, date ] = dateInNum.split("-");
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  console.log(month)
   
   return months[month - 1] + " " + date; 
 }
