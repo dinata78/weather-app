a44624e73abe0582a5a70b8f264f2132e98a6226
dinata78 <stevendinata78@gmail.com>
2024-12-01 21:02:38 +0700

    Add setAdditionalInfo function to set additional-infp's data

---
+++ scripts/setAdditionalInfo.js
@@ -0,0 +1,31 @@
+export { setAdditionalInfo };
+
+function uvIndexMeaning(uvIndex) {
+  if (uvIndex === 0) return "Very Weak";
+  else if (uvIndex <= 2) return "Weak";
+  else if (uvIndex <= 5) return "Moderate";
+  else if (uvIndex <= 7) return "Strong";
+  else if (uvIndex <= 10) return "Very Strong";
+  else if (uvIndex > 10) return "Extreme";
+}
+
+function setAdditionalInfo(uv_index, feels_like, humidity_, chance_of_rain, wind_speed, visibility_, sunrise_time, sunset_time) {
+  const uvIndex = document.querySelector("#uv-index");
+  const feelsLike = document.querySelector("#feels-like");
+  const humidity = document.querySelector("#humidity");
+  const chanceOfRain = document.querySelector("#chance-of-rain");
+  const windSpeed = document.querySelector("#wind-speed");
+  const visibility = document.querySelector("#visibility");
+  const sunriseTime = document.querySelector("#sunrise-time");
+  const sunsetTime = document.querySelector("#sunset-time");
+
+  uvIndex.textContent = uv_index + " " + uvIndexMeaning(uv_index);
+  feelsLike.textContent = feels_like + "°";
+  humidity.textContent = humidity_ + "%";
+  chanceOfRain.textContent = chance_of_rain + "%";
+  windSpeed.textContent = wind_speed + " km/h";
+  visibility.textContent = visibility_ + " km";
+  sunriseTime.textContent = sunrise_time.slice(0, 5);
+  sunsetTime.textContent = sunset_time.slice(0, 5);
+}
+
