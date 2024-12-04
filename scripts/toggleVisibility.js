79fc72203bb41c9b7dc9308afd98329d33409035
dinata78 <stevendinata78@gmail.com>
2024-12-04 15:55:39 +0700

    Add animation effect on currentInfoTime's render

--- scripts/toggleVisibility.js
+++ scripts/toggleVisibility.js
@@ -5,17 +5,20 @@ const toggleVisibility = (() => {
   const hourlyInfo = document.querySelector("#hourly-info");
   const weeklyInfo = document.querySelector("#weekly-info");
   const additionalInfo = document.querySelector("#additional-info");
+  const currentInfoTime = document.querySelector("#current-info-time")
 
   function dayInfo() {
     mainInfo.classList.toggle("hide");
     hourlyInfo.classList.toggle("hide");
     weeklyInfo.classList.toggle("hide");
-    additionalInfo.classList.toggle("hide");  
+    additionalInfo.classList.toggle("hide");
+    currentInfoTime.classList.toggle("hide"); 
   }
 
   function hourInfo() {
     mainInfo.classList.toggle("hide");
-    additionalInfo.classList.toggle("hide");  
+    additionalInfo.classList.toggle("hide");
+    currentInfoTime.classList.toggle("hide"); 
   }
 
   return { dayInfo, hourInfo };
