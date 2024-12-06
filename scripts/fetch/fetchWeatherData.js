d7d62b8e5e3e2d32d312f8b28571bc62aa9e15ec
dinata78 <stevendinata78@gmail.com>
2024-12-06 21:46:21 +0700

    Split API key from fetch url

--- scripts/fetch/fetchWeatherData.js
+++ scripts/fetch/fetchWeatherData.js
@@ -1,7 +1,8 @@
 export { fetchWeatherData };
 
 async function fetchWeatherData(location) {
-  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?unitGroup=metric&key=LYVR2HYS7HN92FEU48Q49BVNF`);
+  const API_KEY = "LYVR2HYS7HN92FEU48Q49BVNF";
+  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next6days?unitGroup=metric&key=${API_KEY}`);
 
   // Handle HTTP errors
   if (!response.ok) {
